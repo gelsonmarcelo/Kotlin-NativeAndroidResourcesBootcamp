0a1acd8bdb7b4e0f9dd0d5e8a1af534709834122
Gelson Nascimento <gelsonschikorski@hotmail.com>
2022-05-08 16:18:02 -0300

    Adiciona código da aula sobre manipulação de contatos

---
+++ app/src/main/java/br/edu/ifc/fraiburgo/nativeandroidresourcesbootcamp/Contact.kt
@@ -0,0 +1,6 @@
+package br.edu.ifc.fraiburgo.nativeandroidresourcesbootcamp
+
+data class Contact(
+    val name: String,
+    val number: String
+)
--- app/src/main/java/br/edu/ifc/fraiburgo/nativeandroidresourcesbootcamp/MainActivity.kt
+++ app/src/main/java/br/edu/ifc/fraiburgo/nativeandroidresourcesbootcamp/MainActivity.kt
@@ -1,10 +1,18 @@
 package br.edu.ifc.fraiburgo.nativeandroidresourcesbootcamp
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.CalendarContract
+import android.provider.ContactsContract
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import br.edu.ifc.fraiburgo.nativeandroidresourcesbootcamp.databinding.ActivityMainBinding
+
+private const val REQUEST_CONTACT_ID = 1
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +23,80 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupListeners()
+        assertSelfPermissionToReadContacts()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setContacts()
+    }
+
+    private fun assertSelfPermissionToReadContacts() {
+        if (!wasContactPermissionGranted()
+        ) {
+            askForContactPermission()
+        }
+    }
+
+    private fun wasContactPermissionGranted(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_CONTACTS
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun askForContactPermission() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(Manifest.permission.READ_CONTACTS),
+            REQUEST_CONTACT_ID
+        )
+    }
+
+    private fun setContacts() {
+        val contactsList: MutableList<Contact> = mutableListOf()
+
+        if (!wasContactPermissionGranted()) {
+            contactsList.add(
+                Contact(
+                    name = "Access to contacts not granted!",
+                    number = "Couldn't to import contacts"
+                )
+            )
+        } else {
+            val cursor = contentResolver.query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,
+                null,
+                null,
+                null
+            )
+
+            if (cursor != null) {
+                while (cursor.moveToNext()) {
+
+                    val displayName = cursor.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
+                    )
+                    val displayNumber = cursor.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.NUMBER
+                    )
+
+                    contactsList.add(
+                        Contact(cursor.getString(displayName), cursor.getString(displayNumber))
+                    )
+                }
+                cursor.close()
+            }
+        }
+
+        val adapter = ContactsAdapter(contactsList)
+
+        binding.contactsRecyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        binding.contactsRecyclerView.adapter = adapter
+    }
+
+    private fun setupListeners() {
         binding.setEventOnCalendar.setOnClickListener {
             val intent = Intent(Intent.ACTION_INSERT)
                 .setData(CalendarContract.Events.CONTENT_URI)
